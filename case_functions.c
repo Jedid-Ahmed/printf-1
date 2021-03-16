b5affe7983d22d0d1a8063675e776efad3437b04
African Coder <78023856+davidmusau@users.noreply.github.com>
2021-03-16 22:30:47 +0300

    Update case_functions.c

--- case_functions.c
+++ case_functions.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * print_character - prints character
  * @arg: va_list parameter
@@ -103,44 +102,4 @@ int print_base16_upper_lower(va_list arg, char *representation)
 	return (cont);
 
 }
-
-/**
- * printf_exclusive_string - print exclusuives string.
- * @val: argumen t.
- * Return: the length of the string.
- */
-
-int printf_exclusive_string(va_list val)
-{
-	char *s;
-	int i, len = 0;
-	int cast;
-
-	s = va_arg(val, char *);
-	if (s == NULL)
-		s = "(null)";
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] < 32 || s[i] >= 127)
-		{
-			_putchar('\\');
-			_putchar('x');
-			len = len + 2;
-			cast = s[i];
-			if (cast < 16)
-			{
-				_putchar('0');
-				len++;
-			}
-			len = len + printf_HEX_aux(cast);
-		}
-		else
-		{
-			_putchar(s[i]);
-			len++;
-		}
-	}
-	return (len);
-}
-
 
