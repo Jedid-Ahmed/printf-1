63c4c555a9de98a9a56293d169de2016e9ae657d
African Coder <78023856+davidmusau@users.noreply.github.com>
2021-03-16 22:52:17 +0300

    Update custom_functions.c

--- custom_functions.c
+++ custom_functions.c
@@ -75,3 +75,23 @@ char *convert_to(char representation[], unsigned int num, int base)
 	} while (num != 0);
 	return (ptr);
 }
+
+int _put_char(char c)
+{ 
+static char buf[1024];
+ static int i;
+  if (c == -1 || i >= 1024)
+ {  
+write(1, &buf, i); 
+ i = 0; 
+} 
+if (c != -1) 
+{ 
+ buf[i] = c;  
+i++;
+ } 
+return (1);
+}
+
+
+
